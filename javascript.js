55d8ea7febf049c7f9a4e007204c7b0f2cc7b5bf
e-ht <1045813+e-ht@users.noreply.github.com>
2021-12-04 20:45:29 -0800

    js wip

--- javascript.js
+++ javascript.js
@@ -10,13 +10,27 @@
 * The observance will be included in the Employee and Labor Relations Manual (ELM) 518, Holiday Leave and be subject to the corresponding provisions of the section.
 */
 
-const usps_holidays = {
-    // '01-01': 'NEW YEARS',
+const holidays = {
+    '01-01': 'New Year\'s day',
+    '01-17': 'Martin Luther King Jr birthday',
+    '02-21': 'Presidents\'s Day',
+    '05-30': 'Memorial Day',
+    '06-20': 'Juneteenth National Independence Day',
+    '07-04': 'Independance Day',
+    '09-05': 'Labor Day',
+    '10-10': 'Columbus Day',
+    '11-11': 'Veterans Day',
+    '11-24': 'Thanksgiving Day',
+    '12-25': 'Christmas Day',
 }
 
 let date = new Date();
-let d = date.getDate();
-let m = date.getMonth();
-let y = date.getFullYear();
+let day = date.getDate();
+let tomorrow = date.setDate(date.getDate() + 1);
+// let yesterday = date.setDate(today - 1);
+// let m = date.getMonth();
+// let y = date.getFullYear();
 
-console.log(d + " " + m + " " + y);
+// console.log(d + " " + m + " " + y);
+
+console.log(day)
