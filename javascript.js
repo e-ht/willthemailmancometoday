474723e427216ca42738d0d90b516563f77ee1a2
e-ht <1045813+e-ht@users.noreply.github.com>
2021-12-06 22:34:59 -0800

    more js tomfoolery

--- javascript.js
+++ javascript.js
@@ -11,35 +11,55 @@
 */
 
 const holidays = {
-    '01-01': 'New Year\'s day',
-    '01-17': 'Martin Luther King Jr birthday',
-    '02-21': 'Presidents\'s Day',
-    '05-30': 'Memorial Day',
-    '06-20': 'Juneteenth National Independence Day',
-    '07-04': 'Independance Day',
-    '09-05': 'Labor Day',
+    '1-01': 'New Year\'s day',
+    '1-17': 'Martin Luther King Jr birthday',
+    '2-21': 'Presidents\'s Day',
+    '5-30': 'Memorial Day',
+    '6-20': 'Juneteenth National Independence Day',
+    '7-04': 'Independance Day',
+    '9-05': 'Labor Day',
     '10-10': 'Columbus Day',
     '11-11': 'Veterans Day',
     '11-24': 'Thanksgiving Day',
+    '12-6': 'j day',
     '12-25': 'Christmas Day',
 }
 
-function willTheMailManCumToday(holidays = {}) {
+function willTheMailManCumToday() {
+    let holiday_today = false;
+    let holiday_tomorrow = false;
+    let holiday_yesterday = false;
+    let friday = false;
+    let saturday = false;
+    let sunday = false;
+    const message = [];
+
     const d = new Date();
-    const isSaturday = d.getDay() == 6 ? true : false;
-    const isSunday = d.getDay() == 0 ? true : false;
-    const day = '0' + d.getDate();
-    day.slice(-2);
-    const month = d.getMonth();
-    const year = d.getFullYear();
+    const today = d.getMonth() + 1 + '-'+ d.getDate();
+    if (today in holidays) {
+        holiday_today = !holiday_today
+        friday = d.getDay() == 5 ? true : false;
+        saturday = d.getDay() == 6 ? true : false;
+        sunday = d.getDay() == 0 ? true : false;
+    }
 
-    // d.setDate(d.getDate() + 1);
-    // let tomorrow = d.getDate();
-    // d.setDate(d.getDate() - 2);
-    // const yesterday = d.getDate();
+    d.setDate(d.getDate() + 1);
+    const tomorrow = d.getMonth() + 1 + '-'+ d.getDate();
+    if (tomorrow in holidays) {
+        holiday_tomorrow = !holiday_tomorrow
+        friday = d.getDay() == 5 ? true : false;
+        saturday = d.getDay() == 6 ? true : false;
+        sunday = d.getDay() == 0 ? true : false;
+    }
 
-
-    console.log(month + " " + day + " " + year);
+    d.setDate(d.getDate() - 2);
+    const yesterday = d.getMonth() + 1 + '-'+ d.getDate();
+    if (yesterday in holidays) {
+        holiday_yesterday = !holiday_yesterday
+        friday = d.getDay() == 5 ? true : false;
+        saturday = d.getDay() == 6 ? true : false;
+        sunday = d.getDay() == 0 ? true : false;
+    }
 }
 
-willTheMailManCumToday(holidays)
+willTheMailManCumToday()
